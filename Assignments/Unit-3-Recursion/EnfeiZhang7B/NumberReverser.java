591f8f4678e8b7002322c74727154b9465dc9507
ecjz01 <ecjz88@gmail.com>
2019-04-11 20:37:42 -0400

    2 done

--- Assignments/Unit-3-Recursion/EnfeiZhang7B/NumberReverser.java
+++ Assignments/Unit-3-Recursion/EnfeiZhang7B/NumberReverser.java
@@ -2,7 +2,7 @@ public class NumberReverser {
     public static int revDigits(int input) {
         if ((int)Math.log10(input) + 1 == 1)
             return input;
-        return (int) ((input % 10) * Math.pow(10, (int) Math.log10(input) + 1)) + revDigits(input / 10);
+        return (int) ((input % 10) * Math.pow(10, (int) Math.log10(input))) + revDigits(input / 10);
     }
 
     public static void main(String[] args) {
